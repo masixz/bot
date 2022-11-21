23e86e4c6d91e2aafd2cbee6b2ab27131689d2e4
Almaz <102tmtacher@gmail.com>
2022-11-21 18:07:56 +0200

    worked on converter

--- converter/converter.py
+++ converter/converter.py
@@ -1,47 +1,46 @@
+from typing import TypedDict
 import pandas as pd
 import json
 import os
 
-datapath = os.path.join(os.path.dirname(__file__), 'Materials.xlsx')
-fn = os.path.join(os.path.dirname(__file__), 'Materials.json')
 
-excel_data_df=pd.read_excel(datapath)
+class Data_Temp(TypedDict):
+    tag: str
+    patterns: list
+    responses: list
+
+
+datapath = os.path.join(os.path.dirname(__file__), "Materials.xlsx")
+fn = os.path.join(os.path.dirname(__file__), "Materials.json")
+fn2 = os.path.join(os.path.dirname(__file__), "intents.json")
+
+excel_data_df = pd.read_excel(datapath)
 excel_data_df.to_json(fn, orient="records")
 with open(fn) as json_file:
     data = json.load(json_file)
-    
-sdy1=data[0]['OBJ_ID_MAT']
-sdy2=data[0]['Object_Name']
-sdy3=data[0]['Dimensions']
-sdy4=data[0]['Unit_Of_Measure']
-sdy5=data[0]['International_Standard']
-sdy6=data[0]['Basic_Material2']
-sdy7=data[0]['Remarks']
-sdy8=data[0]['Mass']
-sdy9=data[0]['Density']
-sdy10=data[0]['eur_kg']
 
+data321 = {"intents": []}
+for i, key in enumerate(data):
 
+    sdy1 = data[i]["OBJ_ID_MAT"]
+    sdy2 = data[i]["Object_Name"]
+    sdy3 = data[i]["Dimensions"]
+    sdy4 = data[i]["Unit_Of_Measure"]
+    sdy5 = data[i]["International_Standard"]
+    sdy6 = data[i]["Basic_Material2"]
+    sdy7 = data[i]["Remarks"]
+    sdy8 = data[i]["Mass"]
+    sdy9 = data[i]["Density"]
+    sdy10 = data[i]["eur_kg"]
 
-sdy=data[0]
-names=[]
-for jvg in sdy:
-    names.append(jvg)
+    m = Data_Temp(tag=sdy1, patterns=[sdy1], responses=[
+                  sdy1, sdy2, sdy3, sdy4, sdy5, sdy6, sdy7, sdy8, sdy9, sdy10])
 
-# lists=[]
-# for i in data:
-#     lists.append(i)
-# print(data)
+    data321["intents"].append(m)
 
-def datan_rakennus():
-    data123 = {"intents": [
-    {"tag": [],
-      "patterns": [],
-      "responses": []}]}
-    # print(data123["intents"][0]["tag"])
-    data123["intents"][0]["tag"] = sdy1
-    data123["intents"][0]["patterns"] = sdy1
-    data123["intents"][0]["responses"] = sdy1,sdy2,sdy3,sdy4,sdy5,sdy6,sdy7,sdy8,sdy9,sdy10
-    lopullinen_data = json.dumps(data123)
-    print(lopullinen_data)
-datan_rakennus()
+with open(fn2, "w") as newjson:
+    json.dump(data321, newjson)
+
+# print(lopullinen_data)
+# with open("intents.json",fn2) as outfile:
+#     outfile.write(lopullinen_data)
